0c698ddb8e686324ea4ec5e2f154986b283711cb
Junsuk Oh <junsuk.oh@survivalcoding.com>
2024-12-04 15:44:20 +0900

    하단 네비게이션 영역 작성

--- lib/core/routing/route_paths.dart
+++ lib/core/routing/route_paths.dart
@@ -2,5 +2,10 @@ abstract class RoutePaths {
   static const String splash = '/Splash';
   static const String signIn = '/SingIn';
   static const String signUp = '/SingUp';
+
+  static const String home = '/Home';
   static const String savedRecipes = '/SavedRecipes';
+  static const String notifications = '/Notifications';
+  static const String profile = '/Profile';
+
 }
--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_recipe_app_course/core/routing/route_paths.dart';
+import 'package:flutter_recipe_app_course/presentation/home/home_screen.dart';
+import 'package:flutter_recipe_app_course/presentation/main/main_screen.dart';
+import 'package:flutter_recipe_app_course/presentation/notifications/notifications_screen.dart';
+import 'package:flutter_recipe_app_course/presentation/profile/profile_screen.dart';
 import 'package:flutter_recipe_app_course/presentation/saved_recipes/screen/saved_recipes_root.dart';
 import 'package:flutter_recipe_app_course/presentation/sign_in/sign_in_screen.dart';
 import 'package:flutter_recipe_app_course/presentation/sign_up/sign_up_screen.dart';
@@ -25,12 +29,56 @@ final router = GoRouter(
       path: RoutePaths.signIn,
       builder: (context, state) => SignInScreen(
         onTapSignUp: () => context.go(RoutePaths.signUp),
-        onTapSignIn: () => context.go(RoutePaths.savedRecipes),
+        onTapSignIn: () => context.go(RoutePaths.home),
       ),
     ),
-    GoRoute(
-      path: RoutePaths.savedRecipes,
-      builder: (context, state) => const SavedRecipesRoot(),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainScreen(
+          body: navigationShell,
+          currentPageIndex: navigationShell.currentIndex,
+          onChangeIndex: (index) {
+            navigationShell.goBranch(
+              index,
+              initialLocation: index == navigationShell.currentIndex,
+            );
+          },
+        );
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.home,
+              builder: (context, state) => const HomeScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.savedRecipes,
+              builder: (context, state) => const SavedRecipesRoot(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.notifications,
+              builder: (context, state) => const NotificationsScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: RoutePaths.profile,
+              builder: (context, state) => const ProfileScreen(),
+            ),
+          ],
+        ),
+      ],
     ),
   ],
 );
